a612ddbbaa7c638b16143026762b193030242bf9
Taro112233 <thanatouchth@gmail.com>
2025-05-16 20:56:21 +0700

    mobile navbar complete

--- src/app/(home)/navbar-sidebar.tsx
+++ src/app/(home)/navbar-sidebar.tsx
@@ -26,11 +26,9 @@ export const NavbarSidebar = ({
         className="p-0 transition-none"
       >
         <SheetHeader className="p-4 border-b">
-          <div className="flex items-center">
-            <SheetTitle>
-              Menu
-            </SheetTitle>
-          </div>
+          <SheetTitle>
+            Menu
+          </SheetTitle>
         </SheetHeader>
         <ScrollArea className="flex flex-col overflow-y-auto h-full pb-2">
           {items.map((item) => (
@@ -38,10 +36,21 @@ export const NavbarSidebar = ({
               key={item.href}
               href={item.href}
               className="w-full text-left p-4 hover:bg-black hover:text-white flex item-center text-base font-medium"
+              onClick={() => onOpenChange(false)}
             >
               {item.children}
             </Link>
           ))}
+          <div className="border-t">
+            <Link
+            onClick={() => onOpenChange(false)} href="/sign-in" className="w-full text-left p-4 hover:bg-black hover:text-white flex item-center text-base font-medium">
+              Log in
+            </Link>
+            <Link
+            onClick={() => onOpenChange(false)} href="/sign-in" className="w-full text-left p-4 hover:bg-black hover:text-white flex item-center text-base font-medium">
+              Start selling
+            </Link>
+          </div>
         </ScrollArea>
       </SheetContent>
     </Sheet>
