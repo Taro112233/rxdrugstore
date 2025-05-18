48de41b52433b89d9017adf6bc68da39370f2d17
Taro112233 <thanatouchth@gmail.com>
2025-05-18 18:50:46 +0700

    add category word

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,9 +1,11 @@
+import { Category } from "@/payload-types";
 import configPromise from "@payload-config";
 import { getPayload } from "payload";
 
 import { Footer } from "./footer";
 import { Navbar } from "./navbar";
 import { SearchFilters } from "./search-filters";
+
 
 interface Props {
   children: React.ReactNode;
@@ -16,13 +18,24 @@ const Layout = async ({ children }: Props) => {
 
   const data = await payload.find({
     collection: "categories",
-    depth: 1,
+    depth: 1, //Populate subcategories, subcategories.[0] will be a type of "Category"
+    pagination: false,
     where: {
       parent: {
         exists: false,
       },
     },
   });
+
+  const formattedData = data.docs.map((doc) => ({
+    ...doc,
+    subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+      //Because of "depth: 1" we are confident doc will be a type of Category
+      ...(doc as Category),
+    }))
+  }))
+
+  console.log(data, formattedData);
 
   return (
     <div className="flex flex-col min-h-screen">
