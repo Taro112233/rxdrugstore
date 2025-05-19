bd17944963f9323d4bd085fd0394918d88555910
Taro112233 <thanatouchth@gmail.com>
2025-05-19 22:00:48 +0700

    Create CustomCategory type
    resolve typescript issue

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,10 +1,10 @@
-import { Category } from "@/payload-types";
 import configPromise from "@payload-config";
 import { getPayload } from "payload";
 
 import { Footer } from "./footer";
 import { Navbar } from "./navbar";
 import { SearchFilters } from "./search-filters";
+import { CustomCategory } from "./type";
 
 interface Props {
   children: React.ReactNode;
@@ -26,15 +26,14 @@ const Layout = async ({ children }: Props) => {
     },
   });
 
-  const formattedData = data.docs.map((doc) => ({
+  const formattedData: CustomCategory[] = data.docs.map((doc) => ({
     ...doc,
     subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
       //Because of "depth: 1" we are confident doc will be a type of Category
-      ...(doc as Category),
+      ...(doc as CustomCategory),
+      subcategories: undefined,
     })),
   }));
-
-  console.log(data, formattedData);
 
   return (
     <div className="flex flex-col min-h-screen">
