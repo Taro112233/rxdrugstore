01bc01883a2e840c4d25c816e3f5ee7ac6b4562d
Taro112233 <thanatouchth@gmail.com>
2025-05-26 00:43:02 +0700

    11.00.37

--- src/modules/tenants/ui/components/navbar.tsx
+++ src/modules/tenants/ui/components/navbar.tsx
@@ -21,3 +21,14 @@ export const Navbar = ({ slug }: Props) => {
     </nav>
   )
 }
+
+export const NavbarSkeleton = () => {
+  return (
+    <nav className="h-20 border-b font-medium bg-white">
+      <div className="max-w-(--breakpoint-xl) mx-auto flex justify-between items-center h-full px-4 lg:px-12">
+        <div />
+        {/* TODO : Skeleton for sheckout button */}
+      </div>
+    </nav>
+  )
+}
