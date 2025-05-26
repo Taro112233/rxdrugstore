70637c5e1aac3ffcc5a6ae393599fd2839cad94b
Taro112233 <thanatouchth@gmail.com>
2025-05-26 16:45:22 +0700

    17.44

--- src/modules/products/ui/components/product-card.tsx
+++ src/modules/products/ui/components/product-card.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { StarIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { generateTenantURL } from "@/lib/utils";
+import { formatCurrency, generateTenantURL } from "@/lib/utils";
 
 interface ProductCardProps {
   id: string;
@@ -69,13 +69,7 @@ export const ProductCard = ({
         </div>
         <div className="p-4">
           <div className="relative px-2 py-1 border bg-pink-400 w-fit">
-            <p className="text-sm font-medium">
-              {new Intl.NumberFormat("en-US", {
-                style: "currency",
-                currency: "THB",
-                maximumFractionDigits: 0,
-              }).format(Number(price))}
-            </p>
+            <p className="text-sm font-medium">{formatCurrency(price)}</p>
           </div>
         </div>
       </div>
--- src/modules/products/ui/views/product-view.tsx
+++ src/modules/products/ui/views/product-view.tsx
@@ -1,4 +1,10 @@
+"use client";
+
+import { formatCurrency, generateTenantURL } from "@/lib/utils";
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import Image from "next/image";
+import Link from "next/link";
 
 interface ProductViewProps {
   productId: string;
@@ -6,16 +12,41 @@ interface ProductViewProps {
 }
 
 export const ProductView = ({ productId, tenantSlug }: ProductViewProps) => {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(
+    trpc.products.getOne.queryOptions({ id: productId })
+  );
+
   return (
     <div className="px-4 lg:px-12 py-10">
       <div className="border rounded-sm bg-white overflow-hidden">
         <div className="relative aspect-[3.9] border-b">
           <Image
-            src="/placeholder.png"
-            alt="cover"
+            src={data.image?.url || "/placeholder.png"}
+            alt={data.name}
             fill
             className="object-cover"
           />
+        </div>
+        <div className="grid grid-cols-1 lg:grid-cols-6">
+          <div className="col-span-4">
+            <div className="p-6">
+              <h1 className="text-4xl font-medium">{data.name}</h1>
+            </div>
+            <div className="border-y flex">
+              <div className="px-6 py-4 flex items-center justify-center border-r">
+                <div className="px-2 py-1 border bg-pink-400 w-fit">
+                  <p className="text-base font-medium">
+                    {formatCurrency(data.price)}
+                  </p>
+                </div>
+              </div>
+
+              <div className="px-6 py-4 flex items-center justify-center lg:border-r">
+                <Link href={generateTenantURL(tenantSlug)}></Link>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
