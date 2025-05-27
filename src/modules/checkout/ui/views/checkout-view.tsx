9a7ee85ddddb8fd4c6054122d529d54d5eae195e
Taro112233 <thanatouchth@gmail.com>
2025-05-27 10:40:57 +0700

    create checkout view

---
+++ src/modules/checkout/ui/views/checkout-view.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+interface CheckoutViewProps {
+  tenantSlug: string;
+}
+
+export const CheckoutView = ({ tenantSlug }: CheckoutViewProps) => {
+  return <div>{tenantSlug}</div>;
+};
