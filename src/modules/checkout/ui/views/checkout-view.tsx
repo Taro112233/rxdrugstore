dbf1593f1e1c24e0aae5601b61b442c338f618e4
Taro112233 <thanatouchth@gmail.com>
2025-05-27 11:12:35 +0700

    Update checkout-view.tsx
    1:28:51

--- src/modules/checkout/ui/views/checkout-view.tsx
+++ src/modules/checkout/ui/views/checkout-view.tsx
@@ -1,9 +1,31 @@
 "use client";
 
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+import { useCart } from "../../hooks/use-cart";
+import { useEffect } from "react";
+import { toast } from "sonner";
+
 interface CheckoutViewProps {
   tenantSlug: string;
 }
 
 export const CheckoutView = ({ tenantSlug }: CheckoutViewProps) => {
-  return <div>{tenantSlug}</div>;
+  const { productIds, clearAllCarts } = useCart(tenantSlug);
+
+  const trpc = useTRPC();
+  const { data, error } = useQuery(
+    trpc.checkout.getProducts.queryOptions({
+      ids: productIds,
+    })
+  );
+
+  useEffect(() => {
+    if (error?.data?.code === "NOT_FOUND") {
+      clearAllCarts();
+      toast.warning("Invalid product found, cart cleared");
+    }
+  }, [error, clearAllCarts]);
+
+  return <div>{JSON.stringify(data, null, 2)}</div>;
 };
