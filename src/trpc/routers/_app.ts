d5339ab1223f9a759737af59bd8e04a924c4d07a
Taro112233 <thanatouchth@gmail.com>
2025-05-31 00:32:55 +0700

    add component in Library

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -6,13 +6,14 @@ import { tenantsRouter } from "@/modules/tenants/server/procedures";
 import { productsRouter } from "@/modules/products/server/procedures";
 import { checkoutRouter } from "@/modules/checkout/server/procedures";
 import { categoriesRouter } from "@/modules/categories/server/procedures";
-
+import { libraryRouter } from "@/modules/library/server/procedures";
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   tags: tagsRouter,
   tenants: tenantsRouter,
   products: productsRouter,
   checkout: checkoutRouter,
+  library: libraryRouter,
   categories: categoriesRouter,
 });
 // export type definition of API
