dd654b4e87969a06480a51c83eb5b1fb5726f35a
Taro112233 <thanatouchth@gmail.com>
2025-06-02 18:03:08 +0700

    3:59:59

---
+++ src/modules/library/ui/components/review-sidebar.tsx
@@ -0,0 +1,18 @@
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+interface Props {
+  productId: string;
+}
+
+export const ReviewSidebar = ({ productId }: Props) => {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(
+    trpc.reviews.getOne.queryOptions({
+      productId,
+    })
+  );
+
+  return;
+  <ReviewForm productId={productId} initialData={data} />;
+};
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,6 +2,7 @@ import { createTRPCRouter } from "../init";
 
 import { authRouter } from "@/modules/auth/server/procedures";
 import { tagsRouter } from "@/modules/tags/server/procedures";
+import { reviewsRouter } from "@/modules/reviews/server/procedures";
 import { tenantsRouter } from "@/modules/tenants/server/procedures";
 import { productsRouter } from "@/modules/products/server/procedures";
 import { checkoutRouter } from "@/modules/checkout/server/procedures";
@@ -11,6 +12,7 @@ export const appRouter = createTRPCRouter({
   auth: authRouter,
   tags: tagsRouter,
   tenants: tenantsRouter,
+  reviews: reviewsRouter,
   products: productsRouter,
   checkout: checkoutRouter,
   library: libraryRouter,
