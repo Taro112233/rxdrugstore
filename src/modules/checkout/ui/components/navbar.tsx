f99f557b08c81384058952520e275b7196ced757
Taro112233 <thanatouchth@gmail.com>
2025-06-14 02:33:26 +0700

    Update navbar.tsx

--- src/modules/checkout/ui/components/navbar.tsx
+++ src/modules/checkout/ui/components/navbar.tsx
@@ -1,7 +1,13 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
-export const Navbar = () => {
+interface NavbarProps {
+  slug: string;
+}
+
+export const Navbar = ({ slug }: NavbarProps) => {
+  // Remove unused parameter warning
+  void slug;
   return (
     <nav className="h-20 border-b font-medium bg-white">
       <div className="max-w-(--breakpoint-xl) mx-auto flex justify-between items-center h-full px-4 lg:px-12">
